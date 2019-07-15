c438ffa9c921575abbe0cb3165b1ff55f0d26009
Umar Junaid <Umar Junaid@172.16.102.233>
2019-07-15 10:38:14 +0500

    with class update

---
+++ maven_compiler_plugins/src/test/java/maven_compiler_plugins/calculatorTest.java
@@ -0,0 +1,14 @@
+package maven_compiler_plugins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
